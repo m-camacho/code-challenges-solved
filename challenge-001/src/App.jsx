c4e6998a1237c320eadb645c11559250c43c684d
Mario Camacho <mario.camacho2@ymail.com>
2025-01-29 15:20:45 -0400

    Passing items to ItemsPanel via props

--- challenge-001/src/App.jsx
+++ challenge-001/src/App.jsx
@@ -1,23 +1,28 @@
-import { useState } from "react";
 import "./App.css";
 import ItemsPanel from "./ItemsPanel";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const items = [
+    { id: "001", label: "first", selected: false },
+    { id: "002", label: "second", selected: false },
+    { id: "003", label: "third", selected: false },
+    { id: "004", label: "fourth", selected: false },
+    { id: "005", label: "fifth", selected: false },
+  ];
 
   return (
     <>
       <h1>Items Columns</h1>
-      <div className="card p-7">
+      <div className="card py-7">
         <div className="container flex gap-2 w-full">
-          <ItemsPanel />
+          <ItemsPanel items={items} />
 
           <div className="buttons-container flex flex-col gap-3 justify-center">
             <button> &#x27A1; </button>
             <button> &#x2B05; </button>
           </div>
 
-          <ItemsPanel />
+          <ItemsPanel items={items} />
         </div>
       </div>
     </>
--- challenge-001/src/ItemsPanel.jsx
+++ challenge-001/src/ItemsPanel.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
 
-function ItemsPanel() {
-  const items = ["first", "second", "third"];
+function ItemsPanel({ items }) {
   return (
-    <div className="flex-1 bg-slate-600 h-40 w-14 rounded">
+    <div className="flex-1 flex flex-col bg-slate-600 h-64 w-14 rounded p-3 gap-2">
       {items.map((item) => (
-        <div>{item}</div>
+        <div key={item.id} className="item bg-slate-400 rounded p-1">
+          {item.label}
+        </div>
       ))}
     </div>
   );
