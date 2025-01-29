e2cd0cec6041ea314e3003f226a56075c6d67980
Mario Camacho <mario.camacho2@ymail.com>
2025-01-29 15:45:18 -0400

    Adding logic to move elements from left to right columns

--- challenge-001/src/App.jsx
+++ challenge-001/src/App.jsx
@@ -1,28 +1,60 @@
+import { useState } from "react";
 import "./App.css";
 import ItemsPanel from "./ItemsPanel";
 
+const items = [
+  { id: "001", label: "first", selected: false },
+  { id: "002", label: "second", selected: false },
+  { id: "003", label: "third", selected: false },
+  { id: "004", label: "fourth", selected: false },
+  { id: "005", label: "fifth", selected: false },
+];
+
 function App() {
-  const items = [
-    { id: "001", label: "first", selected: false },
-    { id: "002", label: "second", selected: false },
-    { id: "003", label: "third", selected: false },
-    { id: "004", label: "fourth", selected: false },
-    { id: "005", label: "fifth", selected: false },
-  ];
+  const [leftItems, setLeftItems] = useState(items);
+  const [rightItems, setRightItems] = useState([]);
 
   return (
     <>
-      <h1>Items Columns</h1>
+      <h1>Items Columns Selector</h1>
       <div className="card py-7">
         <div className="container flex gap-2 w-full">
-          <ItemsPanel items={items} />
+          <ItemsPanel
+            items={leftItems}
+            onSelect={(newItems) => {
+              setLeftItems(newItems);
+            }}
+          />
 
           <div className="buttons-container flex flex-col gap-3 justify-center">
-            <button> &#x27A1; </button>
+            <button
+              onClick={() => {
+                const itemsToStay = [];
+                const itemsToMove = [];
+                leftItems.forEach((currentItem) => {
+                  if (currentItem.selected) {
+                    itemsToMove.push(currentItem);
+                    currentItem.selected = false;
+                  } else {
+                    itemsToStay.push(currentItem);
+                  }
+                });
+                setLeftItems(itemsToStay);
+                setRightItems([...rightItems, ...itemsToMove]);
+              }}
+            >
+              {" "}
+              &#x27A1;{" "}
+            </button>
             <button> &#x2B05; </button>
           </div>
 
-          <ItemsPanel items={items} />
+          <ItemsPanel
+            items={rightItems}
+            onSelect={(newItems) => {
+              setRightItems(newItems);
+            }}
+          />
         </div>
       </div>
     </>
--- challenge-001/src/ItemsPanel.jsx
+++ challenge-001/src/ItemsPanel.jsx
@@ -1,11 +1,23 @@
-import "./App.css";
-
-function ItemsPanel({ items }) {
+function ItemsPanel({ items, onSelect }) {
   return (
     <div className="flex-1 flex flex-col bg-slate-600 h-64 w-14 rounded p-3 gap-2">
-      {items.map((item) => (
-        <div key={item.id} className="item bg-slate-400 rounded p-1">
-          {item.label}
+      {items.map((currentItem) => (
+        <div
+          key={currentItem.id}
+          className={
+            "item rounded p-1 cursor-pointer " +
+            (currentItem.selected ? "bg-slate-400" : "")
+          }
+          onClick={() => {
+            const newItems = [...items];
+            const pressedItem = newItems.find(
+              (listItem) => listItem.id === currentItem.id
+            );
+            pressedItem.selected = !pressedItem.selected;
+            onSelect(newItems);
+          }}
+        >
+          {currentItem.label}
         </div>
       ))}
     </div>
