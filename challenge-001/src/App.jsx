54564594b0096340b7b58bebeeff9e1cf3894ef4
Mario Camacho <mario.camacho2@ymail.com>
2025-01-29 16:26:59 -0400

    Clearing selections on target col

--- challenge-001/src/App.jsx
+++ challenge-001/src/App.jsx
@@ -25,6 +25,9 @@ function App() {
         itemsToStay.push(currentItem);
       }
     });
+    targetList.forEach((currentItem) => {
+      currentItem.selected = false;
+    });
     return [itemsToStay, [...targetList, ...itemsToMove]];
   };
 
