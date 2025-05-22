d9721a6a997ea28b857be17973b852172faf239e
Mario Camacho <mario.camacho2@ymail.com>
2025-05-22 12:31:35 -0400

    Adding basic behavior

--- challenge-004/src/App.jsx
+++ challenge-004/src/App.jsx
@@ -2,27 +2,54 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const { elements } = e.currentTarget;
+    const input = elements.todo;
+    setTodos([
+      ...todos,
+      {
+        id: crypto.randomUUID(),
+        label: input.value,
+        completed: false,
+      },
+    ]);
+    input.value = "";
+  };
 
   return (
     <>
       <div>
         <h1>Todo List</h1>
       </div>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          // You can handle the form submission here
-        }}
-        style={{ marginTop: "1rem" }}
-      >
+      <form onSubmit={onSubmit} style={{ marginTop: "1rem" }}>
         <input
           type="text"
+          name="todo"
           placeholder="Add a todo"
           style={{ marginRight: "0.5rem" }}
         />
         <button type="submit">Add</button>
       </form>
+      <ul>
+        {todos.map((todo) => (
+          <li
+            key={todo.id}
+            className={todo.completed ? "completed" : ""}
+            onClick={() => {
+              setTodos(
+                todos.map((t) =>
+                  t.id === todo.id ? { ...t, completed: !t.completed } : t
+                )
+              );
+            }}
+          >
+            {todo.label}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
